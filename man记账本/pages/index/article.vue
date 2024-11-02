e7fa3395ebbf8b74f86b0a23c34a6d9ba9a7be18
BronLML <2276823919@qq.com>
2024-11-02 16:17:14 +0800

    feat:前端框架模板

---
+++ man记账本/pages/index/article.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="container">
+		<view class="sa-article">
+			<view>{{content}}</view>
+		</view>
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				barBack: getApp().globalData.barBack,
+				barColor: getApp().globalData.barColor,
+				isLoader: true,
+				title: '',
+				content: ''
+			};
+		},
+		onLoad(e) {
+			this.getArticle(e.id);
+		},
+		methods: {
+			getArticle(id) {
+				if(id == 1) {
+					this.content = '添加到桌面';
+				}else{
+					this.content = '关于我们';
+				}
+			}
+		}
+	}
+</script>
+<style lang="less">
+	page {
+		background-color: #fff;
+	}
+	.sa-article {
+		font-size: 32rpx;
+		padding: 30rpx;
+	}
+</style>
